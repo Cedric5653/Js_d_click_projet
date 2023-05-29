b9b64acb6e996fcf98dd52300e3b31b91d165a00
Cédric5653 <cedricnana120@gmail.com>
2023-05-29 12:26:20 +0000

    first commit

---
+++ modale_page/mod.js
@@ -0,0 +1,9 @@
+
+// var msg = document.getElementsByTagName('h1');
+// confirm(msg);
+
+
+function example() {
+    el = document.getElementById("modale");
+    el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
+}
