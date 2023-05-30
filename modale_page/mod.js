2142c5ce954a307e8f1a862e57f7c9fc5fdae8ca
Cédric5653 <cedricnana120@gmail.com>
2023-05-30 09:38:36 +0000

    ice change

--- modale_page/mod.js
+++ modale_page/mod.js
@@ -1,9 +1,15 @@
 
-// var msg = document.getElementsByTagName('h1');
-// confirm(msg);
 
 
 function example() {
     el = document.getElementById("modale");
     el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
 }
+
+
+// function dispa(){
+//     var btn= document.getElementById("btn1");
+
+//     btn.style.display = btn.style.display == "hidden";
+
+// }
